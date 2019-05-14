3041e7772613bf0dbe9c80bdcfd88027c4f2744b
fsanchezgarrido <fsanchez@afi.es>
2019-05-14 17:18:55 +0200

    finalizado

--- index.php
+++ index.php
@@ -1,15 +1,14 @@
 <?php
 require("dbconn.php");
-
 if (isset($_POST["locales"])){
     $locales = $_POST["locales"];
+}
+else{
+    $locales = " ";
+}  
 
-
-}   
-
-$sql = "SELECT * FROM markers WHERE type = 'bar'"
-
-
+$sql = "SELECT * FROM markers WHERE type = '$locales'";
+$result = mysqli_query($conn, $sql);
 
 ?>
 <!DOCTYPE html>
@@ -37,9 +36,9 @@ $sql = "SELECT * FROM markers WHERE type = 'bar'"
 </head>
 
 <body>
-    <div id="options">
-        Elige un tipo de local:
+    <div id="options">       
         <form action="" method="post">
+        Elige un tipo de local:
             <select name="locales" id="locales">
                 <option value="restaurant">Restaurantes</option>
                 <option value="bar">Bares</option>
@@ -52,15 +51,37 @@ $sql = "SELECT * FROM markers WHERE type = 'bar'"
     <div id="map" style="height: 50%; width: 50%"></div>
     <script>
         var map;
-
         function initMap() {
             map = new google.maps.Map(document.getElementById('map'), {
                 center: {
-                    lat: -34.397,
-                    lng: 150.644
+                    lat: -33.898113,
+                    lng: 151.174469
                 },
-                zoom: 8
-            });
+                zoom: 11
+            });  
+
+            <?php
+            $i = 0;            
+            while($fila = mysqli_fetch_array($result)){
+                extract($fila);       
+            ?> 
+                var contentString = '<?php echo "$name"; ?>';
+		        var infowindow<?php echo $i;?> = new google.maps.InfoWindow({
+		    	    content: contentString
+                });                   
+                var myLatLng = {lat:<?php echo "$lat";?>, lng:<?php echo "$lng";?>};
+                var marker<?php echo $i;?> = new google.maps.Marker({
+		            position: myLatLng,
+		            map: map,
+		            title: '<?php echo "$name"; ?>'
+		        });
+                marker<?php echo $i;?>.addListener('click', function() {
+		    	infowindow<?php echo $i;?>.open(map, marker<?php echo $i;?>);
+		  		});
+            <?php
+                $i++;
+            }
+            ?>
         }
     </script>
     <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDSv_CsQYfLsBgcMgoWZvDnxGyDtJudY58&callback=initMap" async defer></script>
